8b3dc85816e83c88fb7bea18381cea0799f75350
Michael Andrews <michael.andrews@pega.com>
2022-07-27 13:20:53 -0400

    created directory structure

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const iquirer = require('inquirer')
+const generatePage = require()
+
+const Employee = require('.lib/Employee')
+const Engineer = require('.lib/Engineer')
+const Intern = require('.lib/Intern')
+const Manager = require('./lib/Manager')
